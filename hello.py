e38a25ef01b7a6b9d2b616c68672b4b19db53acb
Muhamad Ilham Arrouf <ilham.arrouf@gmail.com>
2017-12-14 01:45:50 +0700

    add spesific method

--- hello.py
+++ hello.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello():
     return render_template('hello.html', message='Hello World')
 
-@app.route('/<string:name>')
+@app.route('/<string:name>', methods=['GET'])
 def get_name(name):
     return render_template('hello.html', message='Hello %s' % name)
 
