85f1be8802acb5784cbe357fa2717034c32e13f9
Muhamad Ilham Arrouf <ilham.arrouf@gmail.com>
2017-12-14 01:15:06 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Installation
+```sh
+$ git clone  git@github.com:ilhamarrouf/FlaskBasic-example.git basic
+$ cd basic
+$ . env/bin/activate
+$ python hello.py
+```
---
+++ hello.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('hello.html', message='Hello World')
+
+@app.route('/<string:name>')
+def get_name(name):
+    return render_template('hello.html', message='Hello %s' % name)
+
+if __name__ == '__main__':
+    app.run()
